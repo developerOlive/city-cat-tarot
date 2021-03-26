d9794b6c490a48d171886b99d014a0e40b5bdfaf
developerOlive <chloehiho@gmail.com>
2021-03-26 20:50:26 +0900

    feat : CardRepository를 상속받아 저장소 구현
    전체 카드를 불러오는 경우,
    한 장의 카드 정보만 불러오는 경우에 대해 메소드를 추가하였습니다.

---
+++ app/src/main/java/com/cityCatTarot/domain/MemoryCardRepository.java
@@ -0,0 +1,29 @@
+package com.cityCatTarot.domain;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class MemoryCardRepository implements CardRepository {
+
+    private final EntityManager entityManager;
+
+    public MemoryCardRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Card> findAll() {
+        return entityManager.createQuery("select c from Card c", Card.class)
+                .getResultList();
+    }
+
+    @Override
+    public Optional<Card> findById(int cardId) {
+        Card card = entityManager.find(Card.class, cardId);
+        return Optional.ofNullable(card);
+    }
+}
