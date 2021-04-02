12fdd2f00fec81d228fd2ea096ae6b6d59346108
developerOlive <chloehiho@gmail.com>
2021-04-03 00:57:06 +0900

    feat : HTTP 요청 에러를 처리하기 위한 핸들러 추가

--- app/src/main/java/com/cityCatTarot/controllers/ControllerErrorAdvice.java
+++ app/src/main/java/com/cityCatTarot/controllers/ControllerErrorAdvice.java
@@ -2,6 +2,8 @@ package com.cityCatTarot.controllers;
 
 import com.cityCatTarot.dto.ErrorResponse;
 import com.cityCatTarot.errors.CardNotFoundException;
+import com.cityCatTarot.errors.UserEmailDuplicationException;
+import com.cityCatTarot.errors.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,5 +20,17 @@ public class ControllerErrorAdvice {
     @ExceptionHandler(CardNotFoundException.class)
     public ErrorResponse handleProductNotFound() {
         return new ErrorResponse("Card not found");
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public ErrorResponse handleUserNotFound() {
+        return new ErrorResponse("User not found");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UserEmailDuplicationException.class)
+    public ErrorResponse handleUserEmailIsAlreadyExisted() {
+        return new ErrorResponse("User's email address is already existed");
     }
 }
