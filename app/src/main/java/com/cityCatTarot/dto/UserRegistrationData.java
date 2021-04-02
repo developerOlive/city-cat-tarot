65afeca59b3e9b8c7e1579594fc02fe1590e5eb9
developerOlive <chloehiho@gmail.com>
2021-04-03 01:16:30 +0900

    feat : 회원 가입 시 필요한 정보만 있는 DTO 생성

---
+++ app/src/main/java/com/cityCatTarot/dto/UserRegistrationData.java
@@ -0,0 +1,28 @@
+package com.cityCatTarot.dto;
+
+import com.github.dozermapper.core.Mapping;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 회원 생성 요청 정보.
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistrationData {
+//    @NotBlank
+    @Mapping("email")
+    private String email;
+
+//    @NotBlank
+    @Mapping("nickName")
+    private String nickName;
+
+//    @NotBlank
+    @Mapping("password")
+    private String password;
+}
