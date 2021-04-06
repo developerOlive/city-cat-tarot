5b287ead7ced33712239af603161aecf179afe08
developerOlive <chloehiho@gmail.com>
2021-04-07 02:29:38 +0900

    feat : 로그인 실패 예외처리

---
+++ app/src/main/java/com/cityCatTarot/errors/LoginFailException.java
@@ -0,0 +1,10 @@
+package com.cityCatTarot.errors;
+
+/**
+ * 로그인 실패 예외.
+ */
+public class LoginFailException extends RuntimeException {
+    public LoginFailException(String email) {
+        super("Login fail - email: " + email);
+    }
+}
