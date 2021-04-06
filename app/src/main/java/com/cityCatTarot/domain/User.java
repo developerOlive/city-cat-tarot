5d642f5e675b4eb8d457dde6e7f8f5dba53b5a3b
developerOlive <chloehiho@gmail.com>
2021-04-06 21:04:54 +0900

    feat : User 클래스 - 비밀번호 암호화 관련 메소드 추가

--- app/src/main/java/com/cityCatTarot/domain/User.java
+++ app/src/main/java/com/cityCatTarot/domain/User.java
@@ -26,15 +26,15 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
-    @NotEmpty(message = "회원 이메일은 필수 입니다.")
+    //    @NotEmpty(message = "회원 이메일은 필수 입니다.")
     @Column(name = "user_email")
     private String email;
 
-    @NotEmpty(message = "회원 닉네임은 필수 입니다.")
+    //    @NotEmpty(message = "회원 닉네임은 필수 입니다.")
     @Column(name = "user_nick_name")
     private String nickName;
 
-    @NotEmpty(message = "회원 비밀번호는 필수 입니다.")
+    //    @NotEmpty(message = "회원 비밀번호는 필수 입니다.")
     @Column(name = "user_password")
     private String password;
 
@@ -52,9 +52,31 @@ public class User {
     }
 
     /**
+     * 회원의 비밀번호를 암호화합니다.
+     * @param password 암호화 되지 않은 회원의 비밀번호
+     * @param passwordEncoder 패스워드 인코더
+     */
+    public void changePassword(String password,
+                               PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(password);
+    }
+
+    /**
      * 회원을 삭제합니다.
      */
     public void destroy() {
         deleted = true;
+    }
+
+    /**
+     * 회원이 삭제되지 않았고, 비밀번호가 일치하면 true를 반환합니다.
+     *
+     * @param password 비밀번호
+     * @param passwordEncoder 패스워드 인코더
+     */
+    public boolean authenticate(String password,
+                                PasswordEncoder passwordEncoder) {
+        ;
+        return passwordEncoder.matches(password, this.password);
     }
 }
