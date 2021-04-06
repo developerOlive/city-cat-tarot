d885e09b998bead1c51893d660c6251a56278b20
developerOlive <chloehiho@gmail.com>
2021-04-07 02:29:06 +0900

    feat : 유효하지 않은 토큰 예외처리

---
+++ app/src/main/java/com/cityCatTarot/errors/InvalidTokenException.java
@@ -0,0 +1,10 @@
+package com.cityCatTarot.errors;
+
+/**
+ * 유효하지 않은 토큰 예외.
+ */
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String token) {
+        super("Invalid token: " + token);
+    }
+}
