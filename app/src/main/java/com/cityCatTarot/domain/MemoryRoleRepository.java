c9e7e2343aa6c0dc620057ac7509e69c5e5d6415
developerOlive <chloehiho@gmail.com>
2021-04-06 21:01:56 +0900

    feat : 사용자 권한 저장소 생성 및 인터페이스 구현

---
+++ app/src/main/java/com/cityCatTarot/domain/MemoryRoleRepository.java
@@ -0,0 +1,30 @@
+package com.cityCatTarot.domain;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+public class MemoryRoleRepository implements RoleRepository {
+
+    private final EntityManager entityManager;
+
+    public MemoryRoleRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Role> findAllByUserId(Long id) {
+        return entityManager.createQuery("select r from Role r where u.id = :user_id",
+                Role.class)
+                .setParameter("user_id", id)
+                .getResultList();
+    }
+
+    @Override
+    public Role save(Role role) {
+        entityManager.persist(role);
+        return role;
+    }
+}
