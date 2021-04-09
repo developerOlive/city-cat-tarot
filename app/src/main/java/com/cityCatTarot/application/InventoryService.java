289270282c8d06499262a2d2da8fed61a983e61c
developerOlive <chloehiho@gmail.com>
2021-04-10 05:44:12 +0900

    feat : 카드 보관함 관련 비즈니스 로직을 처리하기 위한 서비스 클래스 생성

---
+++ app/src/main/java/com/cityCatTarot/application/InventoryService.java
@@ -0,0 +1,49 @@
+package com.cityCatTarot.application;
+
+import com.cityCatTarot.domain.Inventory;
+import com.cityCatTarot.domain.InventoryRepository;
+import com.cityCatTarot.domain.UserRepository;
+import com.cityCatTarot.dto.InventorySaveData;
+import com.github.dozermapper.core.Mapper;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class InventoryService {
+
+    private final Mapper mapper;
+    private final InventoryRepository inventoryRepository;
+    private final UserRepository userRepository;
+
+    public InventoryService(Mapper mapper, InventoryRepository inventoryRepository, UserRepository userRepository) {
+        this.mapper = mapper;
+        this.inventoryRepository = inventoryRepository;
+        this.userRepository = userRepository;
+    }
+
+    public Inventory saveCardDetail(Long id, InventorySaveData inventorySaveData,
+                                    Long userId) throws AccessDeniedException {
+
+        return inventoryRepository.save(
+                mapper.map(inventorySaveData, Inventory.class));
+
+    }
+
+    public List<Inventory> findCardListWithUserId(Long userId) {
+        return inventoryRepository.findByUserId(userId);
+    }
+
+//    public List<Inventory> findCardListWithUserIdAndCardId(Long userId, Long cardId) {
+//        List<Inventory> cardListForDelete = inventoryRepository.findByUserIdAndCardId(userId, cardId);
+//
+//        return cardListForDelete;
+//    }
+
+    public void deleteCardDetail(Long inventoryId){
+        inventoryRepository.delete(inventoryId);
+    }
+}
